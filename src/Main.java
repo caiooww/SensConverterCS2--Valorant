50d2d79def19f0a87caeee23ca6b25ff9c9fd771
caiooww <caiomksp@gmail.com>
2025-03-10 19:15:07 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite Sua Sensibilidade de Counter Strike 2:");
+        double sens = sc.nextDouble();
+        double sensCS2 = sens / 3.181818;
+        double sensOW = sens / 10.6;
+        System.out.printf("Sua sensibilidade do CS2 convertida para o Valorant é: %.4f%n", sensCS2);
+
+        sc.close();
+
+    }
+}
