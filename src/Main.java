17f18f37664f66581fa1748756a77c213c95fef8
caiooww <caiomksp@gmail.com>
2025-03-10 19:27:32 -0300

    atualização

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,17 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite Sua Sensibilidade de Counter Strike 2:");
-        double sens = sc.nextDouble();
-        double sensCS2 = sens / 3.181818;
-        double sensOW = sens / 10.6;
-        System.out.printf("Sua sensibilidade do CS2 convertida para o Valorant é: %.4f%n", sensCS2);
+        char resp;
+        do {
+            System.out.println("Digite Sua Sensibilidade de Counter Strike 2:");
+            double sens = sc.nextDouble();
+            double sensCS2 = sens / 3.181818;
+            double sensOW = sens / 10.6;
+            System.out.printf("Sua sensibilidade do CS2 convertida para o Valorant é: %.4f%n", sensCS2);
+            System.out.println("Deseja repetir? (S/N)");
 
+        resp = sc.next().charAt(0);
+       } while (resp != 'n');
         sc.close();
 
     }
